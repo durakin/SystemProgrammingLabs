930358ea49f9b6f3f4fbf50200f49fd06e40ef3b
Nepomniashchii <albertnepomnyashiy@gmail.com>
2021-05-08 04:35:04 +0700

    Lab5 seem to work properly

--- Lab5/serverConsoleInterface.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Albert Nepomnyashiy on 5/8/2021.
-//
-
-#ifndef SERVERSOCKETINTERFACE_H
-#define SERVERSOCKETINTERFACE_H
-
-#endif //SERVERSOCKETINTERFACE_H
--- Lab5/serverSocketInterface.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by Albert Nepomnyashiy on 5/8/2021.
-//
-
-#include "serverConsoleInterface.h"
